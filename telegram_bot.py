b3a6e274f0d83735877d128b8dd8f043851c0ce0
leanr264 <leandroracedo2604@gmail.com>
2025-11-08 18:00:09 -0300

    fixes:se agrego un método de bienvenida en TelegramBot

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,12 +3,24 @@ from image_handler import ImageHandler
 
 
 class TelegramBot:
-    def __init__(self, bot_instance: tlb.TeleBot):
-        self._bot = bot_instance
+    def __init__(self, token):
+        self._bot = tlb.TeleBot(token)
 
     def start(self):
         print("Bot de Telegram IA (POO) iniciado. Esperando mensajes...")
         self._bot.infinity_polling()
+
+    def send_welcome(self, mensaje):
+        """Handler para los comandos /start y /help."""
+        welcome_message = (
+            "**👋 ¡Bienvenido al ChatBot CodexDebug!**\n\n"
+            "Puedo ayudarte con:\n"
+            "1. **Consultas Informaticas**.\n"
+            "2. **Análisis de Sentimientos** (envía un mensaje).\n"
+            "3. **Transcripción de Voz** (envía una nota de voz).\n"
+            "4. **Interpretación de Imágenes** (envía una foto)."
+        )
+        self._bot.reply_to(mensaje, welcome_message, parse_mode='Markdown')
 
     def definir_entrada(self, groq, mensaje):
         if mensaje.photo:
@@ -17,5 +29,5 @@ class TelegramBot:
         respuesta = handler.procesar_entrada(self._bot, mensaje)
         self._bot.send_message(mensaje.chat.id, respuesta)
 
-    def send_response(self, mensaje):
+    def send_response(self, respuesta):
         return
